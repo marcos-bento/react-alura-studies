8cd944301d3659654aeb3dad209b29ff1f7f1acf
Marcos Bento <marcos.x.marcos@hotmail.com>
2023-10-09 11:04:44 -0300

    Finalizado a aula 02 - Componentização

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Formulario from './components/formulario';
+import Lista from './components/lista';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita o arquivo <code>src/App.tsx</code> e salve para recarregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprenda React
-        </a>
-      </header>
+        {<Formulario/>} 
+        {<Lista/>}
     </div>
   );
-}
-
-function oi(){
-  alert("oi");
 }
 
 export default App;
